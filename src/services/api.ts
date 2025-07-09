4cfbabe10e0ad75ccbff0883976c50d3099d4ba8
Manoela Urias <manoela.urias@treetech.com.br>
2025-07-09 11:53:55 -0300

    adiciona logica que comunica com o backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,1 +1,26 @@
 const API_BASE_URL = "http://localhost:5095"
+
+interface Filme {
+    titulo: string;
+    genero: string;
+    duracao: number;
+}
+
+export const FimeService = {
+    async adicionaFilme(filme: Filme): Promise<Filme> {
+        const response = await fetch(`${API_BASE_URL}/api/filmes`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application-json',
+            },
+            body: JSON.stringify(filme),
+        });
+
+        if (!response.ok) {
+            const erroData = await response.json();
+            throw new Error(erroData.message || 'Erro ao adicionar Filme');
+        }
+
+        return response.json();
+    }
+}
